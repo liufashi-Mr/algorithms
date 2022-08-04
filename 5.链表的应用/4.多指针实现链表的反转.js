10d93157c8ba6c4778f6076e59de2b94f85820fd
liufashi <liufashi@yuantutech.com>
2022-08-04 10:12:26 +0800

    延颈秀项，皓质呈露

--- 5.链表的应用/4.多指针实现链表的反转.js
+++ 5.链表的应用/4.多指针实现链表的反转.js
@@ -17,7 +17,7 @@ function ListNode(val, next) {
 
 //- 遍历链表的时候让当前节点next指向上一个节点即可，需要注意next指向改变之后会导致丢失后续的遍历，需要一个next节点用来保存断开的后续节点
 //```js
-const list = new ListNode(
+const list1 = new ListNode(
   1,
   new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
 );
@@ -35,7 +35,7 @@ const reverseList = (list) => {
   }
   return pre;
 };
-console.log(JSON.stringify(reverseList(list)));
+console.log(JSON.stringify(reverseList(list1)));
 //```
 
 //-局部反转一个链表
@@ -46,3 +46,37 @@ console.log(JSON.stringify(reverseList(list)));
 输入: 1->2->3->4->5->NULL, m = 2, n = 4
 输出: 1->4->3->2->5->NULL
 */
+//```js
+const list2 = new ListNode(
+  1,
+  new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+);
+const reverseBetween = (list, m, n) => {
+  let pre, cur, leftHead;
+  const dummy = new ListNode();
+  dummy.next = list;
+  //用于遍历，最初指向dummy
+  leftHead = dummy;
+  // 将p定位到m前面
+  for (let i = 0; i < m - 1; i++) {
+    leftHead = leftHead.next;
+  }
+  // 缓存m前一个节点
+  // start 是反转区间的第一个结点
+  let start = leftHead.next;
+  pre = start;
+  cur = pre.next;
+  for (let i = m; i < n; i++) {
+    let next = cur.next; //先保存next节点，保证遍历能继续进行
+    cur.next = pre; //反转链表
+    // 向后遍历
+    pre = cur;
+    cur = next;
+  }
+  // 反转后将反转区间前面的片段接上去
+  leftHead.next = pre;
+  start.next = cur;
+  return dummy.next;
+};
+console.log(JSON.stringify(reverseBetween(list2, 2, 4)));
+//```
